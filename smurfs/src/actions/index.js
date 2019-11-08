5ef85c711d90611bb51405c075ae79a4cb8edefe
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-11-08 10:17:13 -0600

    Set up store, actions, reducers

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,29 @@
+export const FETCH_SMURFS_LOADING = "FETCH_SMURF_LOADING";
+export const FETCH_SMURFS_SUCCESS = "FETCH_SMURF_SUCCESS";
+export const FETCH_SMURFS_FAILED = "FETCH_SMURF_FAILED";
+
+
+export const smurfLoading = () => ({ type: FETCH_SMURF_LOADING });
+export const smurfLoadSuccess = data => ({ type: FETCH_SMURF_SUCCESS, payload: data });
+export const smurfLoadFailure = error => ({ type: FETCH_SMURF_FAILED, payload: error });
+
+const default_api = "https://localhost:3333/";
+
+export function fetchsmurf(api = default_api) { 
+
+  return function(dispatch) {
+    dispatch(smurfLoading());
+
+    return (
+      fetch(api)
+        .then(response => response.json())
+        .then(json => {
+          console.log(json);
+          dispatch(smurfLoadSuccess(json));
+        })
+        .catch(error => dispatch(smurfLoadFailure(error)))
+    );
+  };
+}
+
+
